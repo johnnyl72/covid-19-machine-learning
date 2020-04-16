8c1c7ac59d5aaba658a9b3d926fa047f46757f71
johnnyl72 <35053246+johnnyl72@users.noreply.github.com>
2020-04-15 17:36:51 -0700

    enable early stop

--- corona_regression.py
+++ corona_regression.py
@@ -1,31 +1,39 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-
-import tensorflow as tf
+import seaborn as sns
 
 from tensorflow import keras
 from tensorflow.keras import layers
 
-import tensorflow_docs as tfdocs
+import tensorflow as tf
 import tensorflow_docs.plots
 import tensorflow_docs.modeling
+import tensorflow_docs as tfdocs
 
-# print(tf.__version__)
+print(tf.__version__)
 
-raw_dataset = pd.read_csv("covid19data_cali3.csv", sep=",", skipinitialspace=True)
+raw_dataset = pd.read_csv("4.14.20_corona_data.csv", sep=",", skipinitialspace=True)
 raw_dataset = raw_dataset[["Total Count Confirmed", "Total Count Deaths"]]
-
 dataset = raw_dataset.copy()
+
+dataset.isna().sum()
+
+dataset = dataset.dropna()
+print(dataset.head())
+print(dataset.tail())
+
 train_dataset = dataset.sample(frac=0.8,random_state=0)
 test_dataset = dataset.drop(train_dataset.index)
+
+sns.pairplot(train_dataset[["Total Count Confirmed", "Total Count Deaths"]], diag_kind="kde")
 
 train_stats = train_dataset.describe()
 train_stats.pop("Total Count Confirmed")
 train_stats = train_stats.transpose()
-print(train_stats)
+train_stats
 
 train_labels = train_dataset.pop('Total Count Confirmed') # Will train this 'y'
-test_labels = test_dataset.pop('Total Count Confirmed') # Our test 'y'
+test_labels = test_dataset.pop('Total Count Confirmed') # Our test 'y' we will use to compare with
 
 def norm(x):
   return (x - train_stats['mean']) / train_stats['std']
@@ -46,18 +54,24 @@ def build_model():
                 metrics=['mae', 'mse'])
   return model
 
-
 model = build_model()
+model.summary()
 
 EPOCHS = 2000
 history = model.fit(
   normed_train_data, train_labels,
   epochs=EPOCHS, validation_split = 0.2, verbose=0,
   callbacks=[tfdocs.modeling.EpochDots()])
-hist = pd.DataFrame(history.history)
-hist['epoch'] = history.epoch
 
 plotter = tfdocs.plots.HistoryPlotter(smoothing_std=2)
+
+plotter.plot({'Basic': history}, metric = "mae")
+plt.ylim([0, 1500])
+plt.ylabel('MAE [Total Count Confirmed]')
+
+plotter.plot({'Basic': history}, metric = "mse")
+plt.ylim([0, 2020000])
+plt.ylabel('MAE [Total Count Confirmed]')
 
 model = build_model()
 # The patience parameter is the amount of epochs to check for improvement
@@ -67,19 +81,18 @@ early_history = model.fit(normed_train_data, train_labels,
                     callbacks=[early_stop, tfdocs.modeling.EpochDots()])
 
 plotter.plot({'Early Stopping': early_history}, metric = "mae")
-plt.ylim([0, 50000])
+plt.ylim([0, 10000])
 plt.ylabel('MAE [Total Count Confirmed]')
 
 loss, mae, mse = model.evaluate(normed_test_data, test_labels, verbose=2)
 print("Testing set Mean Abs Error: {:5.2f} Total Count Confirmed".format(mae))
 
-# Predict
 test_predictions = model.predict(normed_test_data).flatten()
 a = plt.axes(aspect='equal')
 plt.scatter(test_labels, test_predictions)
 plt.xlabel('True Values [Total Count Confirmed]')
 plt.ylabel('Predictions [Total Count Confirmed]')
-lims = [0, 30000]
+lims = [0, 25000]
 plt.xlim(lims)
 plt.ylim(lims)
 _ = plt.plot(lims, lims)
